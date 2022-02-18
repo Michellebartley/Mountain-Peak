4a3251dfd89555117a2589cdedfe6a651a3e562c
Michelle <Michelle.Bartley@dcmail.ca>
2022-02-18 01:28:57 -0500

    Enqueue js

---
+++ themes/mpk/inc/block-editor.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Mountain Peak functions which enhance the theme by utilizing the block editorß
+ *
+ * @package Mountain_Peak
+ */
+
+function mpk_enqueue_block_editor_assets() {
+    wp_enqueue_script(
+        'block-editor-script',
+        get_template_directory_uri().'/assets/js/block-editor.js',
+        array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
+        filemtime( get_template_directory() . '/assets/js/block-editor.js' )
+    );
+}
+add_action( 'enqueue_block_editor_assets', 'mpk_enqueue_block_editor_assets' );
