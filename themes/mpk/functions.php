ca0e579675cda4f4b7acce4c1ec6674319de26f6
Michelle <Michelle.Bartley@dcmail.ca>
2022-03-05 14:09:02 -0500

    changed menu-1 to primary

--- themes/mpk/functions.php
+++ themes/mpk/functions.php
@@ -49,7 +49,7 @@ function mpk_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'mpk' ),
+			'menu-primary' => esc_html__( 'Primary', 'mpk' ),
 		)
 	);
 
