b27bb1a805a16fbaa273009fd14fbf5593d4bfd7
Michelle <Michelle.Bartley@dcmail.ca>
2022-03-07 23:03:16 -0500

    add container and rows into footer

--- themes/mpk/footer.php
+++ themes/mpk/footer.php
@@ -12,12 +12,27 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( ' Copyright © 2022 Mountain Peaks All rights reserved'));
-				?>
-		</div><!-- .site-info -->
+		<div class="site-info col-12 pb-3">
+			<div class="contianer p-2">
+				<div class="row container-xxxl">
+					<div class="col-md-4">
+						<nav id="site-navigation" class="main-navigation align-items-center">
+							<?php
+							wp_nav_menu(
+								array(
+									'theme_location' => 'menu-primary',
+									'menu_id'        => 'primary-menu',
+								)
+							);
+							?>
+						</nav><!-- #site-navigation -->
+					</div>
+				</div>		
+			</div>
+			<?php
+			printf( esc_html__( ' Copyright © 2022 Mountain Peaks All rights reserved'));
+			?>
+		</div><!-- .site-info -->	
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
