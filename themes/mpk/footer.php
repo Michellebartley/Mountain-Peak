90ac1c9d42ea72b781dce6e32753faae30df43e4
Michelle <Michelle.Bartley@dcmail.ca>
2022-03-31 00:57:32 -0400

    Made footer responsive
    added classes and media queries

--- themes/mpk/footer.php
+++ themes/mpk/footer.php
@@ -15,42 +15,29 @@
 
 	<div class="footer-row text-white pt5 pb5">
 		<div class="container col-12">
-			<div class="row">
-				<div class="col-6">
-					<?php dynamic_sidebar( 'footer-widget-col-one' )?>
-				</div>
-				<div class="col-6">
+			<div class="row d-flex">
+				<div class="col-4 sm-6">
 					<?php dynamic_sidebar( 'footer-widget-col-two' )?>
 				</div>
-				<div class="col-6 ms-auto">
+				<div class="col-4 sm-6">
+					<?php dynamic_sidebar( 'footer-widget-col-two' )?>
+				</div>
+				<div class="col-4 sm-12 ms-auto sm-hidden newsletter-signup">
 					<?php dynamic_sidebar( 'footer-widget-col-three' )?>
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<div class="container"></div>
+	<!-- <div class="container"></div> -->
 
 	<div class="container pt-5 pb-3">
 		<div class="row d-flex align-items-center">
-			<div class="col-12">
+			<div class="col-12 copyright">
 				<p>&copy; <?php bloginfo('name');?> <?php echo date('Y'); ?> All rights reserved | Created by <a href="https://michellebartley.ca"><span>Michelle Bartley</span></a> </p>
 			</div>
 		</div>
 	</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
